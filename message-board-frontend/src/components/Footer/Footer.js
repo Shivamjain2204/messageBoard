477d6d9a6376ddba96471bab32a48656add7c304
Shivam <jainshivam2204@gmail.com>
2024-02-10 23:23:21 +0530

    footer

---
+++ message-board-frontend/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="custom-footer text-light text-center py-1 fixed-bottom">
+      <div className="container">
+        <p className="mb-0">All Copyrights © Message Board</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
